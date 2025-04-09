ca110ab29b217742db2a3be015caf07e805674b2
Dileepvasupalli <dileep.vasupalli@infyshine.com>
2025-04-09 22:25:52 +0530

    added filters

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/dileep/shopme/admin/user/UserRepository.java
@@ -0,0 +1,28 @@
+package com.dileep.shopme.admin.user;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.dileep.shopme.common.entity.User;
+
+public interface UserRepository extends PagingAndSortingRepository<User, Integer>{
+
+	@Query("SELECT u From User u WHERE u.email=:email")
+	public User getUserByEmail(@Param("email") String email);
+	
+	public Long countById(Integer id);
+
+	@Query("SELECT u from User u WHERE u.firstName LIKE %?1%OR u.lastName LIKE %?1%OR u.email LIKE %?1%")
+	public Page<User> findAll(String keyword, Pageable pageable);
+	
+	@Modifying
+	@Query("UPDATE  User u SET u.enabled =?2 WHERE u.id= ?1")
+	public void updateEnabledStatus(Integer id , boolean enabled);
+
+	
+}
---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/dileep/shopme/admin/user/UserService.java
@@ -0,0 +1,27 @@
+package com.dileep.shopme.admin.user;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.dileep.shopme.common.entity.Role;
+import com.dileep.shopme.common.entity.User;
+
+public interface UserService {
+
+	public List<User> listAll();
+	
+	public List<Role> listRoles();
+	
+	public User saveUser(User user);
+	
+	public boolean isEmailUnique(Integer id ,String email);
+
+	public User get(Integer id) throws UserNotFoundException;
+	
+	public void delete(Integer id)throws UserNotFoundException;
+	
+	public void updateUserenableStatus(Integer id , boolean enabled);
+
+	public Page<User> listByPage(int pageNum , String sortField , String sortDir,String keyword);
+}
