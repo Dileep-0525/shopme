8dae0a2e240722dede45406549c3ed5e571e2a1e
vasup <vasupallidileep525@gmail.com>
2025-10-03 22:10:31 +0530

    added drop downs

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/dileep/shopme/admin/product/ProductNotFoundException.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/dileep/shopme/admin/product/ProductNotFoundException.java
@@ -2,6 +2,8 @@ package com.dileep.shopme.admin.product;
 
 public class ProductNotFoundException extends Exception{
 
+	private static final long serialVersionUID = -2898722089160615159L;
+
 	public ProductNotFoundException(String message) {
 		super(message);
 	}
